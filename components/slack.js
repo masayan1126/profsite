16ac573dd247ef910e59ef34644d6027f08c2cdd
masaya_nishigaki <67567293+masayan1126@users.noreply.github.com>
2020-11-21 16:33:20 +0900

    Update slack.js

--- components/slack.js
+++ components/slack.js
@@ -1,1 +1,1 @@
-export const webHookUrl = "https://hooks.slack.com/services/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"
+export const webHookUrl = "https://hooks.slack.com/services/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"
