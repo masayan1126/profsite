4ce0eccc8a6b35d63889ab6e7ac5044ba121fc55
masayan1126 <masa199311266@gmail.com>
2020-11-21 16:02:28 +0900

    お問い合わせ画面のレイアウトを調整

--- components/slack.js
+++ components/slack.js
@@ -1,1 +1,1 @@
-export const webHookUrl = "https://hooks.slack.com/services/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"
+export const webHookUrl = "https://hooks.slack.com/services/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"
